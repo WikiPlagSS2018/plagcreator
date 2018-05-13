0bcbf2b8ef766e4f99650f239dadc9a0ce63b46a
KenTacki <KenTacki@users.noreply.github.com>
2018-05-13 18:16:03 +0200

    main work down for create_potential_plagiarism

--- PlagCreator_V2.py
+++ PlagCreator_V2.py
@@ -1,21 +1,21 @@
 import urllib.request
 import random
 import re
+import copy
 
 
 class PlagCreator:
     def __init__(self):
         # get the base text, the plags are gonna be mixed with
         self.base_text = self.get_base_text()
-        # get n articles out of the database, save in list of tuples (docid, text)
-        self.wiki_articles_plag = self.getWikiArticlesFromDB(2, 1)
 
-    def createPlagiarism(self, number_plags):
+    def createPlagiarism(self, number_plagiarism, number_selections, number_plags, start_docid_plags):
 
-        def get_subset_from_base_text(number_selections):
+        def get_subset_from_base_text():
 
             indices_of_sentence_endings = [m.start() for m in re.finditer('\.', self.base_text)]
             selections_from_base_text = list()
+            is_plag = False
 
             for i in range(number_selections):
                 # index within the list, not the text
@@ -24,17 +24,25 @@ class PlagCreator:
                 start_in_base_text = indices_of_sentence_endings[start]
                 end_in_base_text = indices_of_sentence_endings[end]
                 start_end_in_base_text = (start_in_base_text, end_in_base_text)
-                selections_from_base_text.append((start_end_in_base_text[0], start_end_in_base_text[1], self.base_text[start_in_base_text + 2: end_in_base_text + 1]))
+                length_of_selection = end_in_base_text - start_in_base_text + 1
+                selection_id = i + 1
+                selections_from_base_text.append(
+                    (selection_id, is_plag, (start_end_in_base_text[0], start_end_in_base_text[1],
+                                             length_of_selection,
+                                             self.base_text[
+                                             start_in_base_text + 2: end_in_base_text + 1])))
 
             return selections_from_base_text
 
         def get_plags_from_wiki_articles():
 
             plags = list()
+            wiki_articles_plag = self.getWikiArticlesFromDB(number_plags, start_docid_plags)
+            is_plag = True
 
-            for article in self.wiki_articles_plag:
+            for article in wiki_articles_plag:
                 article_docid = article[0]
-                article_text = article[1]
+                article_text = article[1].replace("\n", "")
                 indices_of_sentence_endings = [m.start() for m in re.finditer('[^A-Z0-9]\.(?!\s[a-z])', article_text)]
                 indices_of_sentence_endings = list(filter(
                     lambda x: re.match('[A-Z]', article_text[x + 2]) is not None or re.match('[A-Z]', article_text[
@@ -43,29 +51,71 @@ class PlagCreator:
                 start_plag_index_within_list = random.randint(0, len(indices_of_sentence_endings))
                 end_plag_index_within_list = start_plag_index_within_list + 1
 
-                start_plag_index_within_article = indices_of_sentence_endings[start_plag_index_within_list] + 3 # TODO: check index_out_of_bounds
+                start_plag_index_within_article = indices_of_sentence_endings[
+                                                      start_plag_index_within_list] + 3  # TODO: check index_out_of_bounds
                 end_plag_index_within_article = indices_of_sentence_endings[end_plag_index_within_list]
+                length_of_plag = end_plag_index_within_article - start_plag_index_within_article + 1
 
-                plag = (start_plag_index_within_article, end_plag_index_within_article,
+                plag = (start_plag_index_within_article, end_plag_index_within_article, length_of_plag,
                         article_text[start_plag_index_within_article:end_plag_index_within_article + 1])
 
-                plags.append((article_docid, plag))
+                plags.append((article_docid, is_plag, plag))
 
             return plags
 
-        subset_from_base_text = get_subset_from_base_text(number_plags * 10)
-        plags_from_wiki_articles = get_plags_from_wiki_articles()
+        def create_potential_plagiarism(subset_from_base_text, plags_from_wiki_articles):
+            # TODO: what to do if number_plags > number_selections + 1 (meaning: much more plag than self-made)???
+
+            # compute insertion positions for plags randomly (and begin fill in at end later on)
+            insert_pos_list = sorted(random.sample(range(0, number_selections + 1), number_plags), reverse=True)
+
+            # copy to don't tamper with original subset_from_base_text
+            potential_plagiarisms_mix = copy.copy(subset_from_base_text)
+            # potential_plagiarisms_mix should contain subset_from_base_text and plags of plags_from_wiki_articles
+            plags_pos_index_in_pot_plagiarisms_mix = 0
+            for insert_pos in insert_pos_list:
+                potential_plagiarisms_mix.insert(insert_pos,
+                                                 plags_from_wiki_articles[plags_pos_index_in_pot_plagiarisms_mix])
+                plags_pos_index_in_pot_plagiarisms_mix += 1
+
+            # finally: create potential_plagiarism_text and a list of start-end-tuples of plags inside of it
+            potential_plagiarism_text = ""
+            plags_pos_in_potential_plagiarism_text_list = list()
+            plag_pos_in_potential_plagiarism_text_start = 0
+            for part in potential_plagiarisms_mix:
+                if part[1]:  # meaning: if is_plag...
+                    potential_plagiarism_text += part[2][3] + ". "  # TODO: not too beautiful here
+                    plags_pos_in_potential_plagiarism_text_list.append((plag_pos_in_potential_plagiarism_text_start,
+                                                                        plag_pos_in_potential_plagiarism_text_start +
+                                                                        part[2][2]))
+                    plag_pos_in_potential_plagiarism_text_start += part[2][2]
+                else:
+                    potential_plagiarism_text += part[2][3] + " "  # TODO: not too beautiful here
+                    plag_pos_in_potential_plagiarism_text_start += part[2][2]
+
+            return (plags_pos_in_potential_plagiarism_text_list, potential_plagiarism_text), sorted(
+                plags_from_wiki_articles, reverse=True)
+
+        # compute list of potential_plagiarisms
+        potential_plagiarisms = list()
+        for i in range(number_plagiarism):
+            potential_plagiarisms.append(create_potential_plagiarism(get_subset_from_base_text(),
+                                                                     get_plags_from_wiki_articles()))
+
+        return potential_plagiarisms
 
     def get_base_text(self):
         with open('base_text.txt') as f:
             base_text = f.read()
 
-        return base_text
+        return base_text.replace("\n", "")
 
     def getWikiArticlesFromDB(self, number_plags, start_docid):
         original_wiki_texts = list()
 
-        start_docid = start_docid
+        # just for fun: choose random start_docid if negative value is given
+        if start_docid < 0:
+            start_docid = random.randint(1, 450000)
 
         while len(original_wiki_texts) < number_plags:
             try:
@@ -78,63 +128,8 @@ class PlagCreator:
 
         return original_wiki_texts
 
-    def text_generator_markov(self, min_length, max_length):
-        '''
-        Generates a random text using markov chain. The generated text looks more natural
-        :param number_of_texts: number of texts to be generated
-        :param min_length: min length of the text (lower limit of a random length)
-        :param max_length: max length of the text (upper limit of a random length)
-        :return: list of generated texts; a text is represented as list of words
-        '''
-
-        # randomly choose the index of the word to start with (seed)
-        seed_index = random.randrange(0, len(self.words) - 3)
-        w1, w2 = self.words[seed_index], self.words[seed_index + 1]  # get the word and the next word from the dict
-        text = []
-        length = random.randint(min_length, max_length)
-        for i in range(length):
-            text.append(w1)
-            w1, w2 = w2, random.choice(self.db[(w1, w2)])  # randomly choose one possible word for the selected key
-        text.append(w2)
-
-        return text
-
-    def make_words_list_and_db(self):
-        '''
-        Generates a list of words and dictionary for the markov chain text generator
-        :return: tuple with list of words and dictionary
-        '''
-        # concat all texts of all wiki articles as one bisg list of words
-        print("making words list...")
-        words = []
-        for text in self.wiki_articles.values():
-            words.append(text)
-        words = sum(words, [])  # flatten
-
-        # append the two first words to the end, to avoid KeyError in markov
-        words.append(words[0])
-        words.append(words[1])
-
-        # build triples of three succeeding words with a step size of 1
-        print("making db dictionary...")
-        triples = []
-        if len(words) >= 3:
-            for i in range(len(words) - 2):
-                triple = (words[i], words[i + 1], words[i + 2])
-                triples.append(triple)
-
-        # build a dictionary with a two word key and a list with all succeeding words as the value
-        db = dict()
-        for w1, w2, w3 in triples:
-            key = (w1, w2)
-            if key in db:
-                db[key].append(w3)
-            else:
-                db[key] = [w3]
-        return (words, db)
-
 
 if __name__ == "__main__":
     pc = PlagCreator()
-    pc.createPlagiarism(1)
-    # pc.createPlag(10,1,1,1)
+    plagiarism = pc.createPlagiarism(2, 3, 4, -1)
+    # print(plagiarism)
--- README.md
+++ README.md
@@ -5,14 +5,14 @@ Tool for generate plagiarisms for testing. By default stores the generated plagi
 - python3
 
 ### Install and run:
-1. git clone https://github.com/WikiplagWS17/plagcreator
+1. git clone https://github.com/WikiplagSS2018/plagcreator
 2. python PlagCreator.py
 
 
 21-10-2017 Tested on Windows 10 with python 3.5.2
 
 
-### Manual test of plags (plagsHandmade.json)
+### Manual test of plags
 0. Prerequisites
 
     install HTTPie ```sudo apt-get install httpie```
